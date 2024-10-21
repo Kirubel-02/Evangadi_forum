b75ef69b7d001a4b60cd0fc7e7296524f80d3c80
Kirubel-02 <157580674+Kirubel-02@users.noreply.github.com>
2024-10-21 02:34:00 -0700

    deployment commit

--- client/src/utility/axios.js
+++ client/src/utility/axios.js
@@ -4,8 +4,8 @@ const serverPort = import.meta.env.PORT || 5000;
 
 export const axiosInstance = axios.create({
   //local endpoint reference
-  // baseURL: `http://localhost:${serverPort}/api/v1`,
+  baseURL: `http://localhost:${serverPort}/api/v1`,
 
   // deployed endpoint reference
-  baseURL: "https://evangadiforumproject-vpsk.onrender.com/api/v1",
+  // baseURL: "https://evangadiforumproject-vpsk.onrender.com/api/v1",
 });
